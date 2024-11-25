a5e49a49b2a18b7391583f8f6e352791bee77cc4
wrightrebecca <r3beccawright05@gmail.com>
2024-11-25 12:45:06 +0000

    footer links and rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -6,10 +6,26 @@ function App() {
       <h1>Weather App</h1>
       <p>
         This page was coded by{" "}
-        <a href="https://github.com/wrightrebecca" target="_blank">
+        <a
+          href="https://github.com/wrightrebecca"
+          target="_blank"
+          rel="noreferrer"
+        >
           Rebecca Wright
         </a>
-        . It is available on GitHub and hosted by Netlify.
+        . It is available on{" "}
+        <a
+          href="https://github.com/wrightrebecca/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted by{" "}
+        <a href="#" target="_blank" rel="noreferrer">
+          Netlify
+        </a>
+        .
       </p>
     </div>
   );
