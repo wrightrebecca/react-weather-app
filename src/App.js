37198072262dcb244b39d0d59cada59a13b9bc29
wrightrebecca <r3beccawright05@gmail.com>
2024-11-26 10:49:21 +0000

    Imported Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,34 +3,36 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <p>
-        This page was coded by{" "}
-        <a
-          href="https://github.com/wrightrebecca"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Rebecca Wright
-        </a>
-        . It is available on{" "}
-        <a
-          href="https://github.com/wrightrebecca/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-        and hosted by{" "}
-        <a
-          href="https://app.netlify.com/sites/react-weather-forecaster-project/deploys/6744732604cbb300081c85bc"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-        .
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <p>
+          This page was coded by{" "}
+          <a
+            href="https://github.com/wrightrebecca"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rebecca Wright
+          </a>
+          . It is available on{" "}
+          <a
+            href="https://github.com/wrightrebecca/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted by{" "}
+          <a
+            href="https://app.netlify.com/sites/react-weather-forecaster-project/deploys/6744732604cbb300081c85bc"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+          .
+        </p>
+      </div>
     </div>
   );
 }
